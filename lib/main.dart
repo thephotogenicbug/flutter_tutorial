d4d436a520390696d7456be2771ffb2e8894cd24
thephotogenicbug <navinkumarachari@gmail.com>
2023-08-20 22:42:41 +0530

    Design Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/login_page.dart';
+import 'package:flutter_tutorial/styles/app_color.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Material(
-      child: Text("Hello world"),
-    ),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          fontFamily: 'Urbanist',
+          scaffoldBackgroundColor: AppColors.background),
+      home: const LoginPage(),
+    );
+  }
 }
